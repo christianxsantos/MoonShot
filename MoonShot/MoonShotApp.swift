96215c9e11efcd3fdc6482d98ff72a9cb6fa7725
elunicocondor <cs5176@nyu.edu>
2025-10-25 11:22:58 -0400

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Christian on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
